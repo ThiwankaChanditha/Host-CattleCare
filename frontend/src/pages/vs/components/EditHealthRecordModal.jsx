44ecafed2a0d2afbae67e2574d0f69ae571e4c0a
SThiwanka <thiwankachandithasinhalage@gmail.com>
2025-09-30 17:58:46 +0530

    fixes the issues

--- frontend/src/pages/vs/components/EditHealthRecordModal.jsx
+++ frontend/src/pages/vs/components/EditHealthRecordModal.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import "react-datepicker/dist/react-datepicker.css";
 import { Trash2, X, Plus } from 'lucide-react';
 
 const Modal = ({ isOpen, onClose, title, footer, children, contentClassName = "" }) => {
